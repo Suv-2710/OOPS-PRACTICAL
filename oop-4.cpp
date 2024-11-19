f884cc7ac681c47fc9dc454f96f0b9f1e8a0f818
SUVARNA GADHE <168853756+Suv-2710@users.noreply.github.com>
2024-11-18 16:08:51 -1100

    Update oop-4.cpp

--- oop-4.cpp
+++ oop-4.cpp
@@ -1,34 +1,102 @@
-#include<iostream>
+#include <iostream>
+#include <string> 
 using namespace std;
 
-class student // parent class/base class
-{
-    int rollno;
-    string name, prn;
-    public: 
+class student {
+    int roll_no;
+    string prn_no, name;
+public:
     void accept();
     void display();
+};
+
+class test : virtual public student {
+    string s1[5];
+    int c1[5], e1[5];
+public:
+    void accept_t();
+    void display_t();
+};
+
+class sport : virtual public student {
+    string name_sport;
+    char G;
+public:
+    void accept_s();
+    void display_s();
+};
+
+class result : public test, public sport {
+public:
+    void displayAll() {
+        display();
+        display_t();
+        display_s();
+    }
+} R[10];
+
+void student::accept() {
+    cout << "Enter roll no, PRN no, and name:" << endl;
+    cin >> roll_no >> prn_no >> name;
 }
 
-class test: public student //child class/derived class
-{
-    test t;
-    string c1, c2, c3, c4, c5;
-    float cia_1, cia_2;
-    public:
-    void accept_c();
-    void display_c();
+void student::display() {
+    cout << "\n" << "Roll No" << "\t" << "PRN No" << "\t" << "Name" << endl;
+    cout << roll_no << "\t" << prn_no << "\t" << name << endl;
 }
 
-void student:: accept()
-{
-    cout<<"Enter roll number";
-    cin>>rollno;
-    cout<<"Enter name and PRN";
-    cin>>name>>prn;
+void test::accept_t() {
+    cout << "Enter the Subject Details:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter subject name, CIA marks, and end sem marks:" << endl;
+        cin >> s1[i] >> c1[i] >> e1[i];
+    }
 }
 
-void student:: display()
-{
-    cout<<"\n"<<rollno<<"\t"<<name<<"\t"prn;
+void test::display_t() {
+    cout << "\n" << "Subject Name" << "\t" << "CIA" << "\t" << "ESE" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << s1[i] << "\t" << c1[i] << "\t" << e1[i] << endl;
+    }
+}
+
+void sport::accept_s() {
+    cout << "Enter sport name and grade:" << endl;
+    cin >> name_sport >> G;
+}
+
+void sport::display_s() {
+    cout << "\n" << "Sport Name" << "\t" << "Grade" << endl;
+    cout << name_sport << "\t" << G << endl;
+}
+
+int main() {
+    int ch, choice,n,i;
+    cout<<"enter value of n:";
+    cin>>n;
+    do {
+        cout << "\n1. Accept\n2. Display";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+            for(i=0;i<n;i++)
+            {
+                R[i].accept();
+                R[i].accept_t();
+                R[i].accept_s();
+                break;
+            }
+            case 2:
+                for(i=0;i<n;i++)
+            {
+                R[i].displayAll();
+                break;
+            }
+            
+        }
+        cout << "\nDo you want to continue (0/1)?:";
+        cin >> ch;
+    } while (ch == 1);
+    return 0;
 }
